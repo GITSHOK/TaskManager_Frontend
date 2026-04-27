825f8a96f2adfd32d749b0040b4da88048b99c50
GITSHOK <nishokganapathy07@gmail.com>
2026-04-27 22:39:06 +0530

    sidebar import fixed. again!!!

--- src/pages/Analytics.jsx
+++ src/pages/Analytics.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import Sidebar from "./Sidebar.jsx";
+import Sidebar from "./sidebar.jsx";
 
 import {
   PieChart, Pie, Cell, Tooltip, Legend,
